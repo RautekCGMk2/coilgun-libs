e5fb27056a8f09bd85dcbb919859da2f840cc41c
scott <scottbot95@gmail.com>
2015-09-17 01:19:49 -0700

    Initial commit

---
+++ timing.h
@@ -0,0 +1,30 @@
+/*
+ * timing.h
+ *
+ * Contains all the functions and
+ *
+ *  Created on: Sep 15, 2015
+ *      Author: scott
+ */
+
+#ifndef TIMING_H_
+#define TIMING_H_
+
+typedef struct {
+	int64_t* constants;
+	uint8_t numConstants;
+} polynomial_constants_t;
+
+typedef struct {
+	uint8_t gateNum;
+	polynomial_constants_t velocityPoly;
+} gate_time_t;
+
+typedef struct {
+	uint8_t coilNum;
+	uint32_t timingOverride;
+	uint8_t gateOverride;
+	gate_time_t idealTime;
+} timing_data_t;
+
+#endif /* TIMING_H_ */
